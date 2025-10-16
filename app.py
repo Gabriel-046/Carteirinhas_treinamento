f73dfff35106f17157c8100eb13fdbd3ca715bb4
Gabriel-046 <gabriel.oliveira@inpasa.com.br>
2025-10-16 10:23:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 logo = Image.open("logo.webp")  # Certifique-se de que o nome do arquivo está correto
 st.image(logo, width=200)
 
-st.title("🎓 Carteirinha Digital de Treinamento")
+st.title("Carteirinha Digital de Treinamento")
 
 st.markdown("""
 Preencha **RE** e **Data de Admissão** para ver a carteirinha.  
@@ -110,6 +110,7 @@ if st.button("Consultar"):
             else:
                 st.subheader("Registros encontrados:")
                 st.dataframe(filtro)
+
 
 
 
