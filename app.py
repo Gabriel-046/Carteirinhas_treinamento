0db8960cc199713b60516371f88420e60f8fc470
Gabriel-046 <gabriel.oliveira@inpasa.com.br>
2025-10-16 15:12:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def gerar_carteirinha(nome, re_input, cargo, depto, unidade, treinamentos):
     for treinamento in treinamentos:
         linhas = textwrap.wrap(treinamento, width=max_chars)
         for linha in linhas:
-            draw.text((train_x + 15, current_y), linha, font=font_trein, fill="black")
+            draw.text((train_x + 5, current_y), linha, font=font_trein, fill="black")
             current_y += 35
 
     output_path = "carteirinha_final.png"
@@ -129,6 +129,7 @@ if st.button("Consultar"):
     st.image(imagem_path, caption="Carteirinha Digital", use_container_width=True)
     with open(imagem_path, "rb") as file:
         st.download_button("📥 Baixar Carteirinha", data=file, file_name="carteirinha_final.png", mime="image/png")
+
 
 
 
