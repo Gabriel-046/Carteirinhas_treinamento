56d968d682ac20c5b9ab4ec8b215a6e38c4abde0
Gabriel-046 <gabriel.oliveira@inpasa.com.br>
2025-10-17 09:24:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -92,8 +92,8 @@ def gerar_carteirinha(nome, re_input, cargo, depto, unidade, treinamentos_ordena
     background.save(output_image_path)
 
     # Redimensionar imagem para 5,4 cm x 8,5 cm
-    width_pt = 5.4 * cm
-    height_pt = 8.5 * cm
+    width_pt = 8.5 * cm
+    height_pt = 5.4 * cm
     resized_image = background.resize((int(width_pt), int(height_pt)))
     resized_image_path = "resized_image.png"
     resized_image.save(resized_image_path)
@@ -179,3 +179,4 @@ if st.button("Consultar"):
 
     with open(pdf_path, "rb") as pdf_file:
         st.download_button("📄 Baixar como PDF (5,4cm x 8,5cm)", data=pdf_file, file_name="carteirinha_final.pdf", mime="application/pdf")
+
